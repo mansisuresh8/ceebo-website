f1c72072a90ea0a78bdb2703a5224afed7d158ad
mansisuresh8 <mansijumde0@gmail.com>
2024-03-25 01:01:58 +0530

    swiper js

--- src/components/home/CustomerTestimonials.jsx
+++ src/components/home/CustomerTestimonials.jsx
@@ -1,70 +1,146 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 
 const CustomerTestimonials = () => {
+  const congfig = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    modules: [Autoplay, Pagination],
+    loop: true,
+    autoplay: { delay: 3000 },
+    breakpoints: {
+      800: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  };
   return (
     <div>
       <section className="flex justify-center items-center flex-col lg:flex-row">
-      <div className="flex justify-start items-start flex-col gap-2">
-            <p className="text-3xl font-bold">Customer Testimonials</p>
-            <div className="w-3/4 h-1 bg-orange-600"></div>
-          </div>
+        <div className="flex justify-start items-start flex-col gap-2">
+          <p className="text-3xl font-bold">Customer Testimonials</p>
+          <div className="w-3/4 h-1 bg-orange-600"></div>
+        </div>
       </section>
-      <div className="flex justify-between items-center py-16">
-        <section className="grid grid-cols-1 lg:grid-cols-3 px-8 gap-4">
-          <div className="bg-white rounded-3xl px-8 py-8 items-center">
-            <div className="w-full flex justify-center items-center">
-              <div className="w-36">
-                <img src="./images/CustomerTestimonials-1.png" alt="" className="object-contain w-full"/>
-              </div>
-            </div>
-            <div className="text-center py-8">
-              <p className="font-semibold text-lg">Anita Sandhvi</p>
-              <p className="py-4">
-                “Ceebo Healthmonk has simplified my life by delivering
-                nutritious meals to my doorstep, saving me time and effort.”
-              </p>
-            </div>
-            {/* <div>
-              <img src="./images/star-1.png" alt="" />
-            </div> */}
-          </div>
-          <div className="bg-white rounded-3xl px-8 py-8 items-center">
-          <div className="w-full flex justify-center items-center">
-              <div className="w-36">
-                <img src="./images/CustomerTestimonials-2.png" alt="" className="object-contain w-full"/>
-              </div>
-            </div>
-            <div className="text-center py-8">
-              <p className="font-semibold text-lg">Harsh Mehta</p>
-              <p className="py-4">
-                “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse varius enim in eros elementum tristique. Duis
-                cursus, mi quis viverra ornare..”
-              </p>
-            </div>
-            {/* <div>
-              <img src="./images/star-1.png" alt="" />
-            </div> */}
-          </div>
 
-          <div className="bg-white rounded-3xl px-8 py-8 items-center">
-          <div className="w-full flex justify-center items-center">
-              <div className="w-36">
-                <img src="./images/CustomerTestimonials-3.png" alt="" className="object-contain w-full"/>
-              </div>
-            </div>
-            <div className="text-center py-8">
-              <p className="font-semibold text-lg">Shrushti Nanda</p>
-              <p className="py-4">
-                “Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse varius enim in eros elementum tristique. Duis
-                cursus, mi quis viverra ornare..”
-              </p>
-            </div>
-            {/* <div>
+      <div className="py-16">
+        <section>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            pagination={true}
+            modules={[Autoplay,Pagination]}
+            loop={true}
+            autoplay={{ delay: 3000 }}
+            breakpoints={{
+              950: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+            }}
+          >
+            <SwiperSlide>
+              <div className="bg-white rounded-3xl px-8 py-8 items-center">
+                <div className="w-full flex justify-center items-center">
+                  <div className="w-36">
+                    <img
+                      src="./images/CustomerTestimonials-1.png"
+                      alt=""
+                      className="object-contain w-full"
+                    />
+                  </div>
+                </div>
+                <div className="text-center py-8">
+                  <p className="font-semibold text-lg">Anita Sandhvi</p>
+                  <p className="py-4">
+                    “Ceebo Healthmonk has simplified my life by delivering
+                    nutritious meals to my doorstep, saving me time and effort.”
+                  </p>
+                </div>
+                {/* <div>
               <img src="./images/star-1.png" alt="" />
             </div> */}
-          </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="bg-white rounded-3xl px-8 py-8 items-center">
+                <div className="w-full flex justify-center items-center">
+                  <div className="w-36">
+                    <img
+                      src="./images/CustomerTestimonials-1.png"
+                      alt=""
+                      className="object-contain w-full"
+                    />
+                  </div>
+                </div>
+                <div className="text-center py-8">
+                  <p className="font-semibold text-lg">Anita Sandhvi</p>
+                  <p className="py-4">
+                    “Ceebo Healthmonk has simplified my life by delivering
+                    nutritious meals to my doorstep, saving me time and effort.”
+                  </p>
+                </div>
+                {/* <div>
+              <img src="./images/star-1.png" alt="" />
+            </div> */}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="bg-white rounded-3xl px-8 py-8 items-center">
+                <div className="w-full flex justify-center items-center">
+                  <div className="w-36">
+                    <img
+                      src="./images/CustomerTestimonials-1.png"
+                      alt=""
+                      className="object-contain w-full"
+                    />
+                  </div>
+                </div>
+                <div className="text-center py-8">
+                  <p className="font-semibold text-lg">Anita Sandhvi</p>
+                  <p className="py-4">
+                    “Ceebo Healthmonk has simplified my life by delivering
+                    nutritious meals to my doorstep, saving me time and effort.”
+                  </p>
+                </div>
+                {/* <div>
+              <img src="./images/star-1.png" alt="" />
+            </div> */}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="bg-white rounded-3xl px-8 py-8 items-center">
+                <div className="w-full flex justify-center items-center">
+                  <div className="w-36">
+                    <img
+                      src="./images/CustomerTestimonials-1.png"
+                      alt=""
+                      className="object-contain w-full"
+                    />
+                  </div>
+                </div>
+                <div className="text-center py-8">
+                  <p className="font-semibold text-lg">Anita Sandhvi</p>
+                  <p className="py-4">
+                    “Ceebo Healthmonk has simplified my life by delivering
+                    nutritious meals to my doorstep, saving me time and effort.”
+                  </p>
+                </div>
+                {/* <div>
+              <img src="./images/star-1.png" alt="" />
+            </div> */}
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </section>
       </div>
     </div>
