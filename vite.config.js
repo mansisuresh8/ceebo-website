35c3885afc6983a201903ec32bd0f2179131370e
mansisuresh8 <mansijumde0@gmail.com>
2024-03-28 00:56:08 +0530

    commit update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/ceebo-website",
+  base: "/ceebo-website",
   plugins: [react()],
 })
